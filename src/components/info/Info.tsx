038100f369aa832900b009f1a30f43fb5618ad91
yuexiaoliang <2220124666@qq.com>
2023-01-15 15:54:01 +0800

    first

---
+++ src/components/info/Info.tsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames';
+import { useState, forwardRef, useImperativeHandle, FC, Ref, ForwardRefRenderFunction } from 'react';
+import { Modal } from 'antd';
+
+import styles from './index.module.scss';
+
+interface InfoProps {
+  data: { [key: string]: any };
+}
+
+export interface InfoRef {
+  setVisible: Ref<any>;
+}
+
+const Info: ForwardRefRenderFunction<InfoRef, InfoProps> = (props, ref) => {
+  const data = props.data;
+  const [visible, setVisible] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    setVisible
+  }));
+
+  const footerCom = (
+    <footer key='footer' className={classNames(styles.footer)}>
+      {data.address}
+    </footer>
+  );
+
+  const infoCom = (
+    <ul className={styles.info}>
+      <li>
+        这是我们第<b>{data.index}</b>次一起旅行
+      </li>
+      <li>
+        在这里我们一共去了<b>{data.scenicCount}</b>个景点
+      </li>
+      <li>
+        在这里我们一共拍了<b>{data.photoCount}</b>张照片
+      </li>
+      <li>
+        在这里我们一共消费<b>{data.consume}</b>元
+      </li>
+    </ul>
+  );
+
+  return (
+    <Modal title={data.province + data.city} footer={[footerCom]} open={visible} onCancel={() => setVisible(false)}>
+      {infoCom}
+    </Modal>
+  );
+};
+
+export default forwardRef(Info);
